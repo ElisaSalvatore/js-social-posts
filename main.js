c1a4de4cfba5e30a58fc6a9e6c80fffbfa2fa811
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-01-12 10:45:58 +0100

    Add: function (line 47) to create template Html; Modified: (line) 107 increment counters

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const postsArray = [
         id: 1,
         autore: 'Eric Mangione',
         profilo: './img/profile-picture-1.jpg',
-        data: 'mm-gg-yyyy',
+        data: '2021-06-18',
         testo: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos cumque culpa consequatur esse earum fugiat voluptates error vel. Consequatur exercitationem tenetur inventore sequi. Accusamus, voluptatum! Harum inventore quod explicabo rem!',
         immagine: './img/post-image-1.jpg',
         likes: '80'
@@ -24,7 +24,7 @@ const postsArray = [
         id: 2,
         autore: 'Anna Rougon',
         profilo: './img/profile-picture-2.jpg',
-        data: 'mm-gg-yyyy',
+        data: '2021-10-23',
         testo: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos cumque culpa consequatur esse earum fugiat voluptates error vel. Consequatur exercitationem tenetur inventore sequi. Accusamus, voluptatum! Harum inventore quod explicabo rem!',
         immagine: './img/post-image-2.jpg',
         likes: '40'
@@ -34,27 +34,18 @@ const postsArray = [
         id: 3,
         autore: 'Margaret Smith ',
         profilo: './img/profile-picture-3.jpg',
-        data: 'mm-gg-yyyy',
+        data: '2022-01-04',
         testo: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos cumque culpa consequatur esse earum fugiat voluptates error vel. Consequatur exercitationem tenetur inventore sequi. Accusamus, voluptatum! Harum inventore quod explicabo rem!',
         immagine: './img/post-image-3.jpg',
         likes: '65'
     },
-    
 ];
 
 // Milestone 2
 // Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
 
-//dove andremo a stampare i posts creati?
-const postsContainer = document.querySelector('.posts-list');
-const likeNumber = [];
-
-for (let i = 0; i < postsArray.length; i++) {
-    const post = postsArray[i];
-
-    likeNumber[i] = post.likes;
-    
-    postsContainer.innerHTML += `
+function createTemplateHtml(post) {
+   return ` 
         <div class="post">
             <div class="post__header">
                 <div class="post-meta">                    
@@ -74,7 +65,7 @@ for (let i = 0; i < postsArray.length; i++) {
             <div class="post__footer">
                 <div class="likes js-likes">
                     <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <a class="like-button  js-like-button" href="#">
                             <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                             <span class="like-button__label">Mi Piace</span>
                         </a>
@@ -84,29 +75,57 @@ for (let i = 0; i < postsArray.length; i++) {
                     </div>
                 </div> 
             </div>            
-        </div>
-    `
+        </div> `
+    ;
+};
+
+//dove andremo a stampare i posts creati?
+const postsContainer = document.querySelector('.posts-list');
+const likeNumber = [];
+
+for (let i = 0; i < postsArray.length; i++) {
+    const post = postsArray[i];
+
+    likeNumber[i] = post.likes;
+    
+    postsContainer.innerHTML += createTemplateHtml(post);     
 };
 
 // Milestone 3
 // Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
 // Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
 
-const likeButton = document.querySelectorAll('.js-like-button');
+const likeButton = document.querySelectorAll('.js-like-button'); //ritorn aun array di button
+//assunto: per ogni post c'è un solo pusante like.
+//assunto(2): per ogni post c'è un solo counter.
 
+const likesCounters = document.querySelectorAll('.js-likes-counter');
 const likedPostsArray = [];
 
 for (let i=0; i < likeButton.length; i++) {
-    likeButton[i].addEventListener('click', function() {
-        likeButton[i].classList.add('like-button--liked'); 
+    const button = likeButton[i];
 
-        const likes = document.getElementById(`like-counter-${postsArray[i].id}`);
-        likeNumber[i]++;
-        likes.innerHTML = likeNumber[i];
+    button.addEventListener('click', function (e) {
+        e.preventDefault(); //per evitare lo scatto automatico quando si clicca sul bottone.
+        //"nell'istante in cui sto eseguendo questa azione non far nient'altro se non questo".
 
+        this.classList.add('like-button--liked'); 
+
+        const idPost = i + 1; //TODO
+
+        //salvo in una variabile l'html del counter.
+        const currentCounterHtml = likesCounters[i];
+        //salvo in un'altra variabile il parseInt dell'innerHtml del counter
+        // (così da ottenere il numero intero di likes).
+        const currentCounterInt = parseInt(currentCounterHtml.innerHTML);
+        console.log(currentCounterInt);
+
+        //Ri-salvo (sovrascrivo) l'inner Html con il contatore attuale + 1.
+        currentCounterHtml.innerHTML = currentCounterInt + 1;
+       
+        //Inserisco i post a cui ho messo mi piace in un secondo array.
         likedPostsArray.push(postsArray[i].id);
         console.log(likedPostsArray);
     });
+
 };
-
-
